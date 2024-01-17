97e6a73be10fc0f3bd13e4ff5449585b00d58f78
Mara <samaraore@gmail.com>
2024-01-16 19:10:58 -0500

    Project setting change & removed function
    set main scene as world_test, removed redundant function _ready() from world_test script

--- world_test.gd
+++ world_test.gd
@@ -4,13 +4,6 @@ extends Node2D
 @onready var player := $PlayerCharacter
 @onready var distanceToEnemy = $DistancetoCenter
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if origin.check == true:
 		distanceToEnemy.value = map(100*(origin.position.distance_to(player.position)/origin.radius_size), 100, 0, 0, 100)
