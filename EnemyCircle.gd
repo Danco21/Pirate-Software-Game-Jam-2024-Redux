86e0028a7ac6981a3708af11e588e2932f741c2c
Mara <samaraore@gmail.com>
2024-01-16 21:02:06 -0500

    Clarification on enemy script
    Variables: @onready keyword is redundant for variables that are not nodes, removed.
    
    Comments: added descriptive comments for key variables and functions
    
    Code: redundant * 1 multiplier in draw function, removed

--- EnemyCircle.gd
+++ EnemyCircle.gd
@@ -1,37 +1,40 @@
 extends Node2D
-
+# get collision node, but only when node tree is ready
 @onready var collision = $EnemyRadius/EnemyRadiusCollision
-@onready var radius_size = 10
+# check variables
+var enterValue
+var check
+# starting enemy detection and origin circle
+var radius_size = 10
 var radius_origin = 10
-@onready var enterValue
-@onready var check
 
+# draw sprite for detection circle and origin circle
 func _draw()->void:
-	draw_circle(Vector2(0,0), radius_size*1, Color.BLACK)
+	draw_circle(Vector2(0,0), radius_size, Color.BLACK)
 	draw_circle(Vector2(0,0), radius_origin, Color.RED)
 
-# ? initial radius value of the collision space
+# starting radius value of the collision circle
 func _ready():
 	collision.shape.radius = 0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	# grow shape radius, then refresh the shape drawn on screen
 	radius_size += delta*25
 	queue_redraw()
+	
+	# grow node collision by time elapsed * 10
 	collision.shape.radius += delta*10
 
-
+# test for player sprite entering enemy detection area
 func _on_enemy_radius_body_entered(body):
 	print(body, " has entered Signal Range")
-
 	check = true
-	
+
+# test for player sprite leaving enemy detection area
 func _on_enemy_radius_body_exited(body):
 	check = false
-	
 	print(body, " has exited Signal Range")
 
-
+# test for player sprite reaching origin point
 func _on_enemy_origin_radius_body_entered(body):
 	print(body, " has entered Signal Origin")
-	
