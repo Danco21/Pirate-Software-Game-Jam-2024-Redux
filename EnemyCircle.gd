7bf593e65ec0f0a9b855059075249fded1f7b148
Mara <samaraore@gmail.com>
2024-01-16 21:16:05 -0500

    added growth constant
    added GROWTH_RATE to multiply delta by the same value in multiple places

--- EnemyCircle.gd
+++ EnemyCircle.gd
@@ -8,6 +8,8 @@ var check
 var radius_size = 10
 var radius_origin = 10
 
+const GROWTH_RATE = 10
+
 # draw sprite for detection circle and origin circle
 func _draw()->void:
 	draw_circle(Vector2(0,0), radius_size, Color.BLACK)
@@ -19,11 +21,11 @@ func _ready():
 
 func _process(delta):
 	# grow shape radius, then refresh the shape drawn on screen
-	radius_size += delta*25
+	radius_size += delta * GROWTH_RATE
 	queue_redraw()
 	
 	# grow node collision by time elapsed * 10
-	collision.shape.radius += delta*10
+	collision.shape.radius += delta * GROWTH_RATE
 
 # test for player sprite entering enemy detection area
 func _on_enemy_radius_body_entered(body):
