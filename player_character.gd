4366d0da4a7033ca96ac996ac86155a23cd8f961
Mara <samaraore@gmail.com>
2024-01-16 20:20:20 -0500

    Clarified player movement
    Added comments to player movement, removed calculation for when x/y direction is zero (always resolved to zero)

--- player_character.gd
+++ player_character.gd
@@ -4,20 +4,22 @@ extends CharacterBody2D
 const SPEED = 500.0
 const JUMP_VELOCITY = -400.0
 
-
 func _physics_process(delta):
-	# Get the input direction, axis direction is negative or positive
+	# Get the input direction, negative or positive
 	var direction_x = Input.get_axis("ui_left", "ui_right")
 	var direction_y = Input.get_axis("ui_up", "ui_down")
 	
-	# if (true), velocity on x axis is neg/pos with constant speed
+	# if x-direction non-zero, velocity on x axis is neg/pos * speed constant
 	if direction_x:
 		velocity.x = direction_x * SPEED
+	# if x-direction is zero (input stops), no movement
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = 0
+	# if y-direction non-zero, velocity on x axis is neg/pos * speed constant
 	if direction_y:
 		velocity.y = direction_y * SPEED
+	# if y-direction is zero (input stops), no movement
 	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		velocity.y = 0
 
 	move_and_slide()
